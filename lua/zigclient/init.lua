2dfcaacf0862b4fb200185196c567c83b1432d1c
bfredl <bjorn.linse@gmail.com>
2023-03-20 13:31:29 +0100

    test metadata

--- lua/zigclient/init.lua
+++ lua/zigclient/init.lua
@@ -37,7 +37,7 @@ function h.parse_output(self)
     if #self.data < 8+len then
       return
     end
-    print(kind, len)
+    print("msg:", kind, len)
 
     local body = string.sub(self.data,9, 8+len) -- there's a body alright
     local nxt = string.sub(self.data,9+len)
@@ -46,89 +46,122 @@ function h.parse_output(self)
     local s = h.server_messages
     if kind == s.zig_version then
       self.zig_version = body
+    elseif kind == s.progress then
+      print("progress:", body)
+    elseif kind == s.emit_bin_path then
+      self.bin_path = body
+      if string.sub(self.bin_path, 1, 1) == '\0' then
+        self.bin_path = string.sub(self.bin_path,2)
+      end
     elseif kind == s.error_bundle then
       self.err_body = body
       self.err_bundle = parse_errors(body)
+    elseif kind == s.test_metadata then
+      self.test_meta_body = body
+      self.test_metadata = self.parse_test_metadata(body)
+    elseif kind == s.test_results then
+      self.test_res_body = body
     end
   end
 end
 
 function parse_errors(body)
-    extra_len = u32(body)
-    string_bytes_len = u32(body,1)
-    extra_data = string.sub(body,9,8+extra_len*4)
-    string_data = string.sub(body,8+extra_len*4+1)
-    extra = ffi.cast('uint32_t*', extra_data)
-    string_bytes = ffi.cast('char*', string_data)
+  extra_len = u32(body)
+  string_bytes_len = u32(body,1)
+  extra_data = string.sub(body,9,8+extra_len*4)
+  string_data = string.sub(body,8+extra_len*4+1)
+  extra = ffi.cast('uint32_t*', extra_data)
+  string_bytes = ffi.cast('char*', string_data)
 
-    eml_len = extra[0]
-    eml_start = extra[1]
-    eml_log_text = extra[2]
+  eml_len = extra[0]
+  eml_start = extra[1]
+  eml_log_text = extra[2]
 
-    local function src_loc(src_at, rec)
-      if src_at == 0 then return {} end
-      local reference_trace_len = extra[src_at+7]
-      local srcref_at = src_at+8
-      local ref_trace = {}
-      local ref_hidden = nil
-      for i = 1,reference_trace_len do
-        ref_decl_name = extra[srcref_at+2*(i-1)+0]
-        ref_src_loc = extra[srcref_at+2*(i-1)+1]
-        if ref_src_loc ~= 0 then
-          ref_decl = ffi.string(string_bytes+ref_decl_name)
-          if rec then -- format in theory allows recursive traces, assume such maddnes won't be needed for now
-            ref_src_loc = src_loc(ref_src_loc, false)
-          end
-          table.insert(ref_trace, {decl_name=ffi.string(string_bytes+ref_decl_name), src_loc=ref_src_loc})
-        else
-          ref_hidden = ref_decl_name
+  local function src_loc(src_at, rec)
+    if src_at == 0 then return {} end
+    local reference_trace_len = extra[src_at+7]
+    local srcref_at = src_at+8
+    local ref_trace = {}
+    local ref_hidden = nil
+    for i = 1,reference_trace_len do
+      ref_decl_name = extra[srcref_at+2*(i-1)+0]
+      ref_src_loc = extra[srcref_at+2*(i-1)+1]
+      if ref_src_loc ~= 0 then
+        ref_decl = ffi.string(string_bytes+ref_decl_name)
+        if rec then -- format in theory allows recursive traces, assume such maddnes won't be needed for now
+          ref_src_loc = src_loc(ref_src_loc, false)
         end
+        table.insert(ref_trace, {decl_name=ffi.string(string_bytes+ref_decl_name), src_loc=ref_src_loc})
+      else
+        ref_hidden = ref_decl_name
       end
-      return {
-        src_path = ffi.string(string_bytes+extra[src_at]);
-        line = extra[src_at+1];
-        col = extra[src_at+2];
-        span_start = extra[src_at+3];
-        span_main = extra[src_at+4];
-        span_end = extra[src_at+5];
-        source_line = ffi.string(string_bytes+extra[src_at+6]);
-        ref_trace = ref_trace;
-        ref_hidden = ref_hidden;
-      }
     end
+    return {
+      src_path = ffi.string(string_bytes+extra[src_at]);
+      line = extra[src_at+1];
+      col = extra[src_at+2];
+      span_start = extra[src_at+3];
+      span_main = extra[src_at+4];
+      span_end = extra[src_at+5];
+      source_line = ffi.string(string_bytes+extra[src_at+6]);
+      ref_trace = ref_trace;
+      ref_hidden = ref_hidden;
+    }
+  end
 
-    local function message(msg_at, rec)
-      local msg = {
-        msg = ffi.string(string_bytes+extra[msg_at]);
-        count = extra[msg_at+1];
-        src_loc = src_loc(extra[msg_at+2], true);
-        notes = {}
-      }
-      local notes_len = extra[msg_at+3];
-      local notes_at = msg_at+4
-      for i = 1,notes_len do
-        local note = extra[notes_at+i-1]
-          if rec then -- format in theory allows recursive notes, assume such maddnes won't be needed for now
-            note = message(note, false)
-          end
-          table.insert(msg.notes, note)
-      end
-      return msg
+  local function message(msg_at, rec)
+    local msg = {
+      msg = ffi.string(string_bytes+extra[msg_at]);
+      count = extra[msg_at+1];
+      src_loc = src_loc(extra[msg_at+2], true);
+      notes = {}
+    }
+    local notes_len = extra[msg_at+3];
+    local notes_at = msg_at+4
+    for i = 1,notes_len do
+      local note = extra[notes_at+i-1]
+        if rec then -- format in theory allows recursive notes, assume such maddnes won't be needed for now
+          note = message(note, false)
+        end
+        table.insert(msg.notes, note)
     end
+    return msg
+  end
 
-    local messages = {}
-    for msgid = 1,eml_len do
-      local msg_at = extra[eml_start+msgid-1]
-      table.insert(messages, message(msg_at, true))
-    end
+  local messages = {}
+  for msgid = 1,eml_len do
+    local msg_at = extra[eml_start+msgid-1]
+    table.insert(messages, message(msg_at, true))
+  end
 
-    return messages
+  return messages
+end
+
+function h.parse_test_metadata(body)
+  string_bytes_len = u32(body)
+  tests_len = u32(body,1)
+  name_data = string.sub(body,9,8+tests_len*4)
+  async_frame_len_data = string.sub(body,9+tests_len*4,8+tests_len*8)
+  expected_panic_data = string.sub(body,9+tests_len*8,8+tests_len*12)
+  string_data = string.sub(body,9+tests_len*12,8+tests_len*12+string_bytes_len)
+  string_bytes = ffi.cast('char*', string_data)
+
+  tests = {}
+  for i = 1,tests_len do
+    expected_panic_idx = u32(expected_panic_data, i-1);
+    table.insert(tests, {
+      name = ffi.string(string_bytes+u32(name_data, i-1));
+      async_frame_len = u32(async_frame_len_data, i-1);
+      expected_panic_msg = expected_panic_idx > 0 and ffi.string(string_bytes+expected_panic_idx) or nil;
+    })
+  end
+  return tests
 end
 
 h.__index = h
 
 local uv = vim.loop
-function h.start_server(entrypoint, test)
+function h.start_server(cmd, args)
   local self = setmetatable({}, h)
   self.stdin = uv.new_pipe(false)
 
@@ -136,7 +169,7 @@ function h.start_server(entrypoint, test)
   self.stderr = uv.new_pipe()
   self.stderr:open(self.stderr_hnd.read)
   self.stderr:read_start(vim.schedule_wrap(function(err,data)
-    if data then vim.print(data) end
+    if data then print(data) end
   end))
 
   self.data = nil
@@ -158,6 +191,19 @@ function h.start_server(entrypoint, test)
     vim.schedule(function() self:parse_output() end)
   end)
 
+  vim.print(args)
+
+  self.handle, self.pid = uv.spawn(cmd, {
+    args = args,
+    stdio = {self.stdin, self.stdout_hnd.write, self.stderr_hnd.write}
+  }, vim.schedule_wrap(function()
+    print("server exit")
+  end))
+
+  return self
+end
+
+function h.zig_server(entrypoint, test)
   local subcmd = test and 'test' or 'build-exe'
   args = {
     subcmd;
@@ -165,16 +211,7 @@ function h.start_server(entrypoint, test)
    entrypoint;
     "--listen=-";
   }
-  vim.print(args)
-
-  self.handle, self.pid = uv.spawn("zig", {
-    args = args,
-    stdio = {self.stdin, self.stdout_hnd.write, self.stderr_hnd.write}
-  }, vim.schedule_wrap(function()
-    vim.print("server exit")
-  end))
-
-  return self
+  return h.start_server("zig", args)
 end
 
 function h:send(msg, data)
@@ -182,7 +219,6 @@ function h:send(msg, data)
     msg = h.client_messages[msg]
   end
   data = data or ''
-  msg = 1
   local header = ffi.new("uint32_t[2]")
   header[0] = msg
   header[1] = #data
@@ -191,6 +227,13 @@ function h:send(msg, data)
   if #data > 0 then
     self.stdin:write(data)
   end
+end
+
+function h:run_test(nr)
+  local body = ffi.new("uint32_t[1]")
+  body[0] = nr
+  body = ffi.string(body,4)
+  return self:send(h.client_messages.run_test, body)
 end
 
 return h
--- scratch.lua
+++ scratch.lua
@@ -1,5 +1,22 @@
 mod = loadfile 'lua/zigclient/init.lua' ()
-s = mod.start_server('/home/bfredl/dev/forklift/src/run_ir.zig', false)
+s = mod.zig_server('/home/bfredl/dev/forklift/src/run_ir.zig', false)
+s = mod.zig_server('/home/bfredl/dev/forklift/src/FLIR.zig', true)
 s:send(s.client_messages.update)
+s.bin_path
+t = mod.start_server(s.bin_path, {"--listen=-"})
+t:send(t.client_messages.query_test_metadata)
+t.test_metadata[19+1]
+md = mod.parse_test_metadata(t.test_meta_body)
+#md
+
+string.sub("\0ab", 2)
+t:send(t.client_messages.exit)
+t:run_test(20)
+t
+t.test_res_body
+
+
 s:send(s.client_messages.exit)
-s.err_bundle[1].notes
+s:send(s.client_messages.exit)
+s:send(s.client_messages.query_test_metadata)
+s.err_bundle
